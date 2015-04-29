34706ba7075ecce11acfbbda975c9bbab935b614
rkjc <richardkjcross@gmail.com>
2015-04-29 02:29:24 -0700

    end of night. GridLayout kind of working ... ish

--- CalculatorX/app/src/main/java/com/greyrat/rkjc/calculatorx/MainActivity.java
+++ CalculatorX/app/src/main/java/com/greyrat/rkjc/calculatorx/MainActivity.java
@@ -1,16 +1,25 @@
 package com.greyrat.rkjc.calculatorx;
 
+import java.util.Random;
+
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private Button oneKey;
+    Random rnd = new Random();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,20 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        oneKey = (Button)findViewById(R.id.button7);
+        oneKey.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                int colR = rnd.nextInt(255);
+                int colG = rnd.nextInt(255);
+                int colB = rnd.nextInt(255);
+                oneKey.setBackgroundColor(Color.rgb(colR, colG, colB));
+
+            }
+        });
+
         return true;
     }
 
@@ -42,6 +65,16 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static void disableSoftInputFromAppearing(EditText editText) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+            editText.setTextIsSelectable(true);
+        } else {
+            editText.setRawInputType(InputType.TYPE_NULL);
+            editText.setFocusable(true);
+        }
     }
 
 
